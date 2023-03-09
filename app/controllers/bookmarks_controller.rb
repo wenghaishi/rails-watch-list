aa0635f930b53b3f6363927c1e109a65e8691605
weng haishi <wenghaishi1@gmail.com>
2023-03-09 16:19:01 +0800

    implemented index method for lists controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,10 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,11 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+  
 end
