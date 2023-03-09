488490eba4694b07bc5dfa4baa41e1c0ad880818
weng haishi <wenghaishi1@gmail.com>
2023-03-10 01:53:48 +0800

    created destroy method for bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,6 +5,22 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(params[:list_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id).merge(list_id: params[:list_id])
   end
 end
